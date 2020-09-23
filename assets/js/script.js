f1b92421bf8697ac2828437d939d8aca8121644a
amyusup <amyusup26@gmail.com>
2020-09-23 23:44:54 +0700

    update.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,12 @@ function onFocusEvent(index) {
         }
     }
 }
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
